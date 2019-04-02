23c55df0be48e6e99df1e886931661811f5ea3e1
tloth <toth.annar@gmail.com>
2019-04-02 19:45:49 +0100

    required fs and added if for endpoint'/'

--- src/server.js
+++ src/server.js
@@ -1,10 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 
 const message = "goodie goodie";
 function handler(request, response){
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message);
-    response.end();
+    const method = request.method;
+    console.log(method);
+
+    const endpoint = request.url;
+    console.log(endpoint);
+
+    if (endpoint === "/") {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+            if (error) {
+                console.log(error);
+                return;
+// What are we returning here?
+            };
+        response.end(file);
+        })
+    } else if (endpoint === "/node") {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("It's the node!");
+        response.end();
+    } else if (endpoint === "/girls") {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("It's the girls!!!");
+        response.end();
+    } else {    
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(message);
+        response.end();
+    }
 }
 
 const server = http.createServer(handler);
